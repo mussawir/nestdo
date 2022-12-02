b0a915ce3d1faa71b1e614ea55c1d7ccd5c1eaaf
shahrukhnasir <shahrukhnasir0108@gmail.com>
2022-12-02 17:23:54 +0500

    update

--- src/ProjectCategories/projectCat.model.ts
+++ src/ProjectCategories/projectCat.model.ts
@@ -4,36 +4,7 @@ import { Document } from 'mongoose';
 @Schema()
 export class ProjectCat {
   @Prop()
-  Art:string;
-  @Prop()
-  Comics:string;
-  @Prop()
-  Crafts:string; 
-  @Prop()
-  Dance :string;
-  @Prop()
-  Design:string;
-  @Prop()
-  Fashion:string;
-  @Prop()
-  Film:string;
-  @Prop()
-  Food:string;
-  @Prop()
-  Games:string;
-  @Prop()
-  Journalism:string;
-  @Prop()
-  Music:string;
-  @Prop()
-  Photography:string;
-  @Prop()
-  Publishing:string;
-  @Prop()
-  Technology:string;
-  @Prop()
-  Theater:string;
-
+  name: string;
 }
 
 export type ProjectModule = ProjectCat & Document;
