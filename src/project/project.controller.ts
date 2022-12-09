0f56a4b5beae11c976b419e8a065f9f0488441e3
mussawir <mussawir20@gmail.com>
2022-12-09 22:02:02 +0500

    update

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, ValidationPipe } from '@nestjs/common';
  //import { Project } from './Project.model';
 import { ProjectService } from './project.service';
 import { ProjectDto } from './project.dto';
@@ -10,6 +10,18 @@ export class ProjectController {
 constructor(
     private readonly ProjectService:ProjectService
 ){}
+// @Get()
+// @Get('find?:bFind')
+//   async get(@Param('bFind') bFind: boolean): Promise<Country> {
+//     return this.projectService.getSearch(bFind);
+//   }
+
+@Get('find?:bFind')
+find(@Param('bfind')){
+    // return this.ivService.GetAllData()
+  return {"name": "ali"}; 
+
+}
 
     @Post()
     Project(@Body() ProjectDto:ProjectDto): any{
