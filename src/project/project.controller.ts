a5ec308fba721487d78094e4f5431dbaf00b5ef4
mussawir <mussawir20@gmail.com>
2022-12-14 21:37:56 +0500

    updates

--- src/project/project.controller.ts
+++ src/project/project.controller.ts
@@ -11,10 +11,10 @@ import {
     Patch,
     Req
   } from '@nestjs/common';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { Express } from 'express';
+// import { FileInterceptor } from '@nestjs/platform-express';
+// import { Express } from 'express';
 import { CreateProjectDto } from './dto/create-project.dto';
-import { UpdateProjectDto } from './dto/update-project.dto';
+// import { UpdateProjectDto } from './dto/update-project.dto';
 import { UpdateLocationDto } from './dto/up-location.dto';
 import { Project } from './schemas/project.schema';
 import { ProjectService } from './project.service';
@@ -22,9 +22,9 @@ import { UpdateBasicDto } from './dto/up-basic.dto';
 import { UpdateStoryDto } from './dto/up-story.dto';
 import { UpdateMyteamDto } from './dto/up-myteam.dto';
 import { UpdatePaymentMethodDto } from './dto/up-paymentmethod.dto';
-import { diskStorage } from 'multer';
-import { extname } from 'path';
-import aws from "aws-sdk";
+// import { diskStorage } from 'multer';
+// import { extname } from 'path';
+// import aws from "aws-sdk";
 
 
 @Controller('project')
@@ -85,36 +85,5 @@ export class ProjectController {
         return this.projectService.updatePaymentMethod(projectId, updatePaymentMethodDto);
     }
 
-    // @Post('upload')
-    // @UseInterceptors(FileInterceptor('file'))
-    // uploadFile(@UploadedFile() file: Express.Multer.File) {
-    //   console.log(file);
-    //   return file;
-    // }
-
-   
-    // @Post('fileupload')
-    // @UseInterceptors(FileInterceptor('file'))
-    // fileUpload(@UploadedFile() file: Express.Multer.File) {
-    //   const s3 = new aws.S3({
-    //     endpoint: "nyc3.digitaloceanspaces.com",
-    //     accessKeyId: "DO00FM4ZCTYDCCMRHGTU",
-    //     secretAccessKey: "agrtXp8vSvQwyw3wuYvpQUXI8QFttVGHK8cSXacy9es",
-    //   });
-    //   const name = new type(arguments);
-    //   // s3.upload({
-    //   //   Bucket: "ivspace", // Add bucket name here
-    //   //   ACL: "public-read", // Specify whether anyone with link can access the file
-    //   //   Key: `test/test.png`, // Specify folder and file name
-    //   //   Body: file,
-    //   // }, {
-    //   //   partSize: 10 * 1024 * 1024,
-    //   //   queueSize: 10,
-    //   // }).send((err) => {
-    //   //   if (err) return "Error";
-    //   //   });
-    //   console.log(file);
-    //   return file;
-    // }
 
 }
