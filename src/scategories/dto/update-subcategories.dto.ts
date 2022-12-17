87b03e1670f7419c680113f70a905ceb17d86aea
mussawir <mussawir20@gmail.com>
2022-12-17 22:28:42 +0500

    next

---
+++ src/scategories/dto/update-subcategories.dto.ts
@@ -0,0 +1,3 @@
+export class UpdateSubCategoryDto {
+    name: string;
+}
---
+++ src/scategories/schemas/subcategories.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from 'mongoose';
+
+export type SubCategoryDocument = SubCategory & Document;
+
+@Schema()
+export class SubCategory {
+    @Prop()
+    subcategoryId: string;
+
+    @Prop()
+    name: string;
+
+    @Prop()
+    categoryId: string;
+}
+export const SubCategorySchema = SchemaFactory.createForClass(SubCategory);
