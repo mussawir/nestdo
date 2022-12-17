2f69ba66e6aee77dbe07e3434bcced08f5450d8a
mussawir <mussawir20@gmail.com>
2022-12-17 23:00:02 +0500

    up

--- src/scategories/scategories.service.ts
+++ src/scategories/scategories.service.ts
@@ -12,23 +12,23 @@ export class SCategoriesService{
         return this.subCategoriesRepository.findOne({ subcategoryId })
     }
 
-    // async getSubCategoriesByCat(categoryId: string): Promise<SubCategory[]> {
-    //     return this.subCategoriesRepository.findByCat({ categoryId })
-    // }
+    async getSubCategoriesByCat(categoryId: string): Promise<SubCategory[]> {
+        return this.subCategoriesRepository.findByCat({ categoryId })
+    }
 
-    // async getSubCategories(): Promise<SubCategory[]> {
-    //     return this.subCategoriesRepository.find({});
-    // }
+    async getSubCategories(): Promise<SubCategory[]> {
+        return this.subCategoriesRepository.find({});
+    }
 
-    // async createSubCategory(name: string, categoryId: string): Promise<SubCategory> {
-    //     return this.subCategoriesRepository.create({
-    //         subcategoryId: uuidv4(),
-    //         name,
-    //         categoryId
-    //     })
-    // }
+    async createSubCategory(name: string, categoryId: string): Promise<SubCategory> {
+        return this.subCategoriesRepository.create({
+            subcategoryId: uuidv4(),
+            name,
+            categoryId
+        })
+    }
 
-    // async updateSubCategory(subcategoryId: string, subCategoryUpdates: UpdateSubCategoryDto): Promise<SubCategory> {
-    //     return this.subCategoriesRepository.findOneAndUpdate({ subcategoryId }, subCategoryUpdates);
-    // }
+    async updateSubCategory(subcategoryId: string, subCategoryUpdates: UpdateSubCategoryDto): Promise<SubCategory> {
+        return this.subCategoriesRepository.findOneAndUpdate({ subcategoryId }, subCategoryUpdates);
+    }
 }
