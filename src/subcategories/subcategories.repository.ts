0deb3562a08f651de6b08379dfbd0f679fc43cea
mussawir <mussawir20@gmail.com>
2022-12-17 18:37:27 +0500

    one comment

--- src/subcategories/subcategories.repository.ts
+++ src/subcategories/subcategories.repository.ts
@@ -1,7 +1,6 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { FilterQuery, Model } from "mongoose";
-
 import { SubCategory, SubCategoryDocument } from "./schemas/subcategories.schema";
 
 @Injectable()
